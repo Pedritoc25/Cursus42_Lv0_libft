89fecd4f0ec137728b3c1b12ab0407bd98b2c7cc
Pedro Cabanas Rumayor <pcabanas@c2r4s4.42madrid.com>
2024-02-01 15:41:46 +0100

    aprobbed isascii

--- ft_isascii.c
+++ ft_isascii.c
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isascii.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pcabanas <pcabanas@student.42madrid>       +#+  +:+       +#+        */
+/*   By: pcabanas <pcabanas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 15:14:05 by pcabanas          #+#    #+#             */
-/*   Updated: 2024/01/09 15:37:10 by pcabanas         ###   ########.fr       */
+/*   Updated: 2024/02/01 15:30:26 by pcabanas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//checks whether c fits into the ASCII character set
+//checks whether char fits into the ASCII character set
 int	ft_isascii(int c)
 {
-	return (c >= 0 && c <= 128);
+	return (c >= 0 && c <= 127);
 }
 /*#include<stdio.h>
 #include<ctype.h>
