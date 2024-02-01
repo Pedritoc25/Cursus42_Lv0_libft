d2abed1930ed5a9b17502ba9580f41662ac2d8e3
Pedro Cabanas Rumayor <pcabanas@c2r4s4.42madrid.com>
2024-02-01 15:40:43 +0100

    aprobbed isalpha

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isalpha.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pcabanas <pcabanas@student.42madrid>       +#+  +:+       +#+        */
+/*   By: pcabanas <pcabanas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 10:43:19 by pcabanas          #+#    #+#             */
-/*   Updated: 2024/01/09 15:28:20 by pcabanas         ###   ########.fr       */
+/*   Updated: 2024/01/29 10:55:34 by pcabanas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//isalpha - check whether a character is alphabetical
+//check whether a character is alphabetical
 int	ft_isalpha(int c)
 {
 	return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
