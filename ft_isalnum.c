ab43404f9b915c90ba94a1f507e66d0ff657c0ac
Pedro Cabanas Rumayor <pcabanas@c2r4s4.42madrid.com>
2024-02-01 15:41:11 +0100

    aprobbed isalnum

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,13 +6,13 @@
 /*   By: pcabanas <pcabanas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 13:26:58 by pcabanas          #+#    #+#             */
-/*   Updated: 2024/01/17 10:15:40 by pcabanas         ###   ########.fr       */
+/*   Updated: 2024/01/31 16:33:16 by pcabanas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//isalnum - check whether a character is alphanumeric
 #include "libft.h"
 
+//check whether a character is alphanumeric
 int	ft_isalnum(int c)
 {
 	return (ft_isalpha(c) || ft_isdigit(c));
