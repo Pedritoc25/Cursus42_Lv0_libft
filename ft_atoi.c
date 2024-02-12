8c957fd17b174a203e1e67ed26f48f1a414b1830
Pedro Cabanas Rumayor <pcabanas@c1r10s4.42madrid.com>
2024-02-12 10:57:10 +0100

    atoi corregido

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,25 +6,22 @@
 /*   By: pcabanas <pcabanas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 11:54:22 by pcabanas          #+#    #+#             */
-/*   Updated: 2024/01/31 18:02:53 by pcabanas         ###   ########.fr       */
+/*   Updated: 2024/02/12 10:53:45 by pcabanas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-//convert a string to an integer
-static int	check_spaces(int i, const char *nptr)
+//convert a string to an longeger
+static long	check_spaces(long i, const char *nptr)
 {
-	if (nptr[0] == ' ' || nptr[0] == '\f' || nptr[0] == '\n'
-		|| nptr[0] == '\r' || nptr[0] == '\t' || nptr[0] == '\v')
-	{
-		while (nptr[i] == ' ')
-			i++;
-	}
+	while (nptr[i] == ' ' || nptr[i] == '\f' || nptr[i] == '\n'
+		|| nptr[i] == '\r' || nptr[i] == '\t' || nptr[i] == '\v')
+		i++;
 	return (i);
 }
 
-static int	get_number(int i, const char *nptr, int *digit, int *number)
+static long	get_number(long i, const char *nptr, long *digit, long *number)
 {
 	while (ft_isdigit(nptr[i]))
 	{
@@ -35,52 +32,40 @@ static int	get_number(int i, const char *nptr, int *digit, int *number)
 	return (i);
 }
 
-int	check_value(const char nptr, int value, int *i)
+long	check_value(const char nptr, long value, long *i)
 {
-	if (nptr == ' ' || nptr == '\f' || nptr == '\n'
-		|| nptr == '\r' || nptr == '\t' || nptr == '\v')
-		value = 3;
-	else if (nptr == '-' && value == 0)
+	if (nptr == '-')
+	{
 		value = -1;
-	else if (nptr == '+' && value == 0)
-		value = 1;
-	*i = *i + 1;
+		*i = *i + 1;
+	}
+	else if (nptr == '+')
+		*i = *i + 1;
 	return (value);
 }
 
 int	ft_atoi(const char *nptr)
 {
-	int	i;
-	int	digit;
-	int	value;
-	int	number;
+	long	i;
+	long	digit;
+	long	value;
+	long	number;
 
 	i = 0;
-	value = 0;
+	value = 1;
 	digit = 0;
 	number = 0;
 	i = check_spaces(i, nptr);
-	while (nptr[i] != '\0' && value != 3)
-	{
-		value = check_value(nptr[i], value, &i);
-		if (!ft_isdigit(nptr[i]) && value != 0)
-			return (value * number);
-		else if (ft_isdigit(nptr[i]))
-		{
-			if (value == 0)
-				value = 1;
-			i = get_number(i, nptr, &digit, &number);
-			return (number * value);
-		}
-		i++;
-	}
-	return (number * value);
+	value = check_value(nptr[i], value, &i);
+	while (ft_isdigit(nptr[i]))
+		i = get_number(i, nptr, &digit, &number);
+	return ((int)(number * value));
 }
 /*#include <stdlib.h>
-int	main()
+long	main()
 {
 	//const char nptr[]= "      +56w0e";
 	const char nptr[]= "2";
-	printf("%i\n%i\n", ft_atoi(nptr), atoi(nptr));
-	//printf("%i\n", atoi(nptr));
+	prlongf("%i\n%i\n", ft_atoi(nptr), atoi(nptr));
+	//prlongf("%i\n", atoi(nptr));
 }*/
