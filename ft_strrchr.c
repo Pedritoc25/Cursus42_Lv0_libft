90214201e498db9a820057b2d0fcde2131eb1451
Pedro Cabanas Rumayor <pcabanas@c1r10s4.42madrid.com>
2024-02-12 10:04:23 +0100

    strrchr corregido

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: pcabanas <pcabanas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 17:50:13 by pcabanas          #+#    #+#             */
-/*   Updated: 2024/02/01 17:03:50 by pcabanas         ###   ########.fr       */
+/*   Updated: 2024/02/12 10:02:23 by pcabanas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,35 @@
 //locate character in string starting at end
 char	*ft_strrchr(const char *s, int c)
 {
-	size_t		len;
+	size_t	len;
 
 	len = ft_strlen(s);
-	while (len >= 0)
+	if (c == '\0')
+		return ((char *)&s[len]);
+	if (len > 0)
+		len--;
+	else
+		return (NULL);
+	while (len > 0)
 	{
 		if (s[len] == (char)c)
 			return ((char *)&s[len]);
 		len--;
 	}
+	if (s[len] == (char)c)
+		return ((char *)&s[len]);
 	return (NULL);
 }
 /*#include <string.h>
 int	main(void)
 {
+	char *src = "\0";
+ 	char *d1 = strrchr(src, 'a');
+ 	char *d2 = ft_strrchr(src, 'a');
 	const char	s[] = "teste";
 	const char	t[] = "teste";
 	int			c = 'x';
 	printf("%s\n%s\n", ft_strrchr(s, c), strrchr(t, c));
+	printf("%s\n%s\n", d2, d1);
 	return (0);
 }*/
