fe2cf7cc546cddda1eeb6158e66cb88eee7574cb
Pedro Cabanas Rumayor <pcabanas@c1r4s3.42madrid.com>
2024-02-20 12:14:41 +0100

    formateo split mejorado

--- ft_split.c
+++ ft_split.c
@@ -6,12 +6,26 @@
 /*   By: pcabanas <pcabanas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 14:16:37 by pcabanas          #+#    #+#             */
-/*   Updated: 2024/02/16 12:35:58 by pcabanas         ###   ########.fr       */
+/*   Updated: 2024/02/20 12:14:13 by pcabanas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <string.h>
+
+static long	word_count(char const *s, char c, size_t len, size_t i)
+{
+	while (s[i] != '\0')
+	{
+		if (s[i] != c)
+		{
+			len++;
+			while (s[i] != c && s[i] != '\0')
+				i++;
+		}
+		i++;
+	}
+	return (len);
+}
 
 //split a string into a series of tokens based on a particular delimiter.
 char	**ft_split(char const *s, char c)
@@ -21,7 +35,6 @@ char	**ft_split(char const *s, char c)
 	size_t	i;
 	size_t	j;
 	char	**result;
-	//char	*temp;
 
 	len = 0;
 	word_len = 0;
@@ -29,24 +42,7 @@ char	**ft_split(char const *s, char c)
 	j = 0;
 	if (!s)
 		return (NULL);
-	//printf ("s[25] -> %s\n", s); //BORRAR
-	//printf ("c[26] -> %c\n", c); //BORRAR
-	while (s[i] != '\0')
-	{
-		if (s[i] != c)
-		{
-			len++;
-			while (s[i] != c && s[i] != '\0')
-			{
-				//printf ("Posicion[33] -> %c\n", s[i]); //BORRAR
-				i++;
-			}
-			//printf ("NºPablabras -> %zu\n", len); //BORRAR
-		}
-		i++;
-	}
-	//printf ("\n\n\n\n"); //BORRAR
-	i = 0;
+	len = word_count(s, c, len, i);
 	result = (char **)ft_calloc(len + 1, sizeof(char *));
 	if (!result)
 		return (NULL);
@@ -56,14 +52,10 @@ char	**ft_split(char const *s, char c)
 		{
 			while (s[i] != c && s[i] != '\0')
 			{
-				//printf ("Posicion[33]2 -> %c\n", s[i]); //BORRAR
 				word_len++;
 				i++;
 			}
-			//printf ("Len_Pablabra2 -> %zu\n", word_len); //BORRAR
-			//printf ("Len para el temp calloc -> %zu\n", (word_len + 1) * sizeof(char)); //BORRAR
-			//temp = (char *)ft_calloc(word_len + 1, sizeof(char));
-			result[j] = ft_substr(s, i-word_len, word_len);
+			result[j] = ft_substr(s, i - word_len, word_len);
 			if (!result[j])
 			{
 				while (j >= 0)
@@ -72,28 +64,16 @@ char	**ft_split(char const *s, char c)
 					j--;
 				}
 				free(result);
-    			return (NULL);
+				return (NULL);
 			}
-			//printf ("Cuanto mide temp -> %zu\n", ft_strlen(temp)); //BORRAR
-			//word_len = ft_strlcpy(temp, &s[i - word_len], word_len + 1);
-			//printf ("Cuanto mide temp2 -> %zu\n", ft_strlen(temp)); //BORRAR
-			//printf ("Cuanto vale temp -> %s\n", temp); //BORRAR
-			//result[j] = temp;
-			//printf ("Posicion Result[%zu] -> %s\n", j ,result[j]); //BORRAR
 			word_len = 0;
 			j++;
 		}
 		i++;
 	}
-	//printf ("Posicion S[56] -> %c\n", s[i]); //BORRAR
-	//printf ("Posicion Result[%zu] -> %s\n", j ,result[j]); //BORRAR
-	//printf ("\n\n\n\n"); //BORRAR
 	i = 0;
 	while (result[i] != '\0')
-	{
-		//printf ("%s ", result[i]); //BORRAR
 		i++;
-	}
 	return (result);
 }
 
