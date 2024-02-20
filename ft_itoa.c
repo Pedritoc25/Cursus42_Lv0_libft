55e50e210e0eceb9f5b17e49432bc76e282f5b08
Pedro Cabanas Rumayor <pcabanas@c1r4s3.42madrid.com>
2024-02-20 12:04:21 +0100

    itoa norminette working

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,26 +6,14 @@
 /*   By: pcabanas <pcabanas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 10:22:08 by pcabanas          #+#    #+#             */
-/*   Updated: 2024/02/20 10:50:16 by pcabanas         ###   ########.fr       */
+/*   Updated: 2024/02/20 12:03:22 by pcabanas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-//convert the integer n into a character string.
-char	*ft_itoa(int n)
+static int	calculate_int_len(int n2, int len)
 {
-	char	*result;
-	int		len;
-	int		n2;
-
-	n2 = n;
-	len = 0;
-
-	if (n2 == 0)
-		return (result = ft_strdup("0"));
-	if (n2 == -2147483648)
-		return (result = ft_strdup("-2147483648"));
 	if (n2 < 0)
 	{
 		n2 = -n2;
@@ -36,7 +24,23 @@ char	*ft_itoa(int n)
 		n2 /= 10;
 		len++;
 	}
+	return (len);
+}
+
+//convert the integer n into a character string.
+char	*ft_itoa(int n)
+{
+	char	*result;
+	int		len;
+	int		n2;
+
 	n2 = n;
+	len = 0;
+	if (n2 == 0)
+		return (result = ft_strdup("0"));
+	if (n2 == -2147483648)
+		return (result = ft_strdup("-2147483648"));
+	len = calculate_int_len(n2, len);
 	if (n2 < 0)
 		n2 = -n2;
 	result = (char *)ft_calloc(len + 1, sizeof (char));
@@ -53,9 +57,9 @@ char	*ft_itoa(int n)
 	return (result);
 }
 
-int    main(void)
+/*int    main(void)
 {
     int    i = 10101;
 	printf("i = %d | %s\n", i, ft_itoa(i));
     return (0);
-}
+}*/
