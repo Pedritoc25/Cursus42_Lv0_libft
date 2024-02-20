0a02e2f097c5034640238da6d2fef0aa23364636
Pedro Cabanas Rumayor <pcabanas@c1r3s5.42madrid.com>
2024-02-20 10:51:24 +0100

    itoa funciona bien

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: pcabanas <pcabanas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 10:22:08 by pcabanas          #+#    #+#             */
-/*   Updated: 2024/02/19 13:29:51 by pcabanas         ###   ########.fr       */
+/*   Updated: 2024/02/20 10:50:16 by pcabanas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,39 +22,40 @@ char	*ft_itoa(int n)
 	n2 = n;
 	len = 0;
 
-	if (n = 0)
+	if (n2 == 0)
+		return (result = ft_strdup("0"));
+	if (n2 == -2147483648)
+		return (result = ft_strdup("-2147483648"));
+	if (n2 < 0)
 	{
-		//strdub
-		return ("0");
-	}
-	if (n = -2147483648)
-	{
-		//strdub
-		return ("-2147483648");
-	}
-	while(n2 > 0)
-	{
-		if (n < 0)
-		{
-			n2 = -n2;
-			len++;
-		}
-		n2 %= 10;
+		n2 = -n2;
 		len++;
 	}
+	while (n2 > 0)
+	{
+		n2 /= 10;
+		len++;
+	}
+	n2 = n;
+	if (n2 < 0)
+		n2 = -n2;
 	result = (char *)ft_calloc(len + 1, sizeof (char));
+	if (!result)
+		return (NULL);
 	while (len)
 	{
-		result[len - 1] = (n % 10);
+		result[len - 1] = (n2 % 10) + '0';
+		n2 /= 10;
 		len--;
 	}
-
+	if (n < 0)
+		result[0] = '-';
 	return (result);
 }
 
-int	main(void)
+int    main(void)
 {
-	int	i = 100;
-	printf(ft_itoa(i));
-	return (0);
+    int    i = 10101;
+	printf("i = %d | %s\n", i, ft_itoa(i));
+    return (0);
 }
