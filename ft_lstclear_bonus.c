2eb6f2758e0d8c913bbfcb5b2d96d5aed5ce4323
Pedro <pedritoc25@gmail.com>
2024-03-05 11:57:19 +0100

    lstclear main added

--- ft_lstclear_bonus.c
+++ ft_lstclear_bonus.c
@@ -6,13 +6,13 @@
 /*   By: pcabanas <pcabanas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 10:37:12 by pcabanas          #+#    #+#             */
-/*   Updated: 2024/03/05 11:44:21 by pcabanas         ###   ########.fr       */
+/*   Updated: 2024/03/05 11:56:16 by pcabanas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-//
+//Deletes and frees the given node and every succesor of that node
 void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
 	t_list	*current;
@@ -32,5 +32,20 @@ void	ft_lstclear(t_list **lst, void (*del)(void*))
 
 /*int	main(void)
 {
+	t_list	*node;
+	t_list	*node2;
+	t_list	*node3;
+
+	node = malloc(sizeof(t_list));
+	node2 = malloc(sizeof(t_list));
+	node3 = malloc(sizeof(t_list));
+	if (!node && !node2 && !node3)
+	{
+		free(node);
+		free(node2);
+		free(node3);
+		return (0);
+	}
+	ft_lstclear(&node, &free);
 	return (0);
 }*/
