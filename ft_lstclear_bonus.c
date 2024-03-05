7beb8c535f6bdf10cbfb9983a3dfeea96fa57f0c
Pedro <pedritoc25@gmail.com>
2024-03-05 11:45:17 +0100

    lstclear finished

--- ft_lstclear_bonus.c
+++ ft_lstclear_bonus.c
@@ -6,7 +6,7 @@
 /*   By: pcabanas <pcabanas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 10:37:12 by pcabanas          #+#    #+#             */
-/*   Updated: 2024/03/05 10:38:12 by pcabanas         ###   ########.fr       */
+/*   Updated: 2024/03/05 11:44:21 by pcabanas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,19 @@
 //
 void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
+	t_list	*current;
+	t_list	*temp;
 
+	if (!lst || !del)
+		return ;
+	current = *lst;
+	while (current)
+	{
+		temp = current;
+		current = current->next;
+		ft_lstdelone(temp, del);
+	}
+	*lst = NULL;
 }
 
 /*int	main(void)
