930c18f59a2f358ba3788f61cace494c4c9a6a76
P25 <pedritoc25@gmail.com>
2024-07-01 17:23:32 +0200

    calloc and strjoin formated

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,57 +6,45 @@
 /*   By: pcabanas <pcabanas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 12:24:15 by pcabanas          #+#    #+#             */
-/*   Updated: 2024/02/26 16:53:21 by pcabanas         ###   ########.fr       */
+/*   Updated: 2024/07/01 17:20:29 by pcabanas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	*ft_mystrcat(const char *dst, const char *src)
+//combine elements
+char	*ft_strjoin(char const *dst, char const *src)
 {
 	size_t	i;
 	char	*s;
-	size_t	s_len;
 
+	if (!dst || !src)
+		return (NULL);
 	s = (char *)ft_calloc((ft_strlen(dst) + ft_strlen(src) + 1), sizeof(char));
 	if (!s)
-	{
 		return (NULL);
-	}
 	i = 0;
-	s_len = 0;
 	while (dst[i] != '\0')
 	{
 		s[i] = dst[i];
 		i++;
 	}
-	s_len = ft_strlen(s);
-	i = 0;
-	while (src[i] != '\0')
+	while (*src != '\0')
 	{
-		s[s_len + i] = src[i];
+		s[i] = *src;
 		i++;
+		src++;
 	}
-	return (s);
-}
-
-//combine elements
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	char	*s;
-
-	if (!s1 || !s2)
-		return (NULL);
-	s = ft_mystrcat(s1, s2);
 	if (s == NULL)
 		return (NULL);
 	else
 		return ((char *)s);
 }
+
 /*int	main(void)
 {
-	char const	s1[]  = "abc";
-	char const	s2[]  = "abc";
+	char const	s1[]  = "ac";
+	char const	s2[]  = "ab";
 	char		*result = ft_strjoin(s1, s2);
 	printf("%s\n", result);
 }*/
